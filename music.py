dc0488a11c5e0a91167f19bd90cfcb15d93f7ee9
Nagesh Kharat <130240484+nageshkharat@users.noreply.github.com>
2025-01-19 22:40:55 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import filedialog   
 from pygame import mixer  
-root = Tk()
+root = Tk() 
 root.title('Mp3 player')    
 mixer.init()     
 songs_list=Listbox(root,bg="violet",fg="dark blue",font='arial 15',height=12, width=47,selectmode=SINGLE,selectbackground='brown',selectforeground='white')
